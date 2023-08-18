eda27697f9e76348c9df0a50d9ee6ae45833a937
lovelindhoni <jblovelindhoni@gmail.com>
2023-08-18 11:45:14 +0530

    Set build target to 'esnext' in vite.config.ts to support top-level await in store.ts.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	build: {
+		target: 'esnext',
 		rollupOptions: {
 			output: {
 				// added config for manual chunks for reducing build size
