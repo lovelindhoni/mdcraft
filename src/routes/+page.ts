a59a74a1ebae0054c86bce035b4bddac47dd8084
lovelindhoni <jblovelindhoni@gmail.com>
2023-09-26 21:19:16 +0530

    Added the snippet for Vercel Analytics in +page.ts
    - mdcraftv.015

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,2 +1,8 @@
+// For vercel analytics...
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
+inject({ mode: dev ? 'development' : 'production' });
+
+// I had to because then only indexdeb works good. Biggest decision I have made in this project.
 export const ssr = false;
-// i had to because then only indexdeb works good. Biggest decision i have made in this project.
